2eb2abb4a29a5b00dc75ceac535088e08bdb069e
lieby weintraub <liebahinda@gmail.com>
2019-07-31 17:19:16 +0000

    Automatically backed up by Learn

--- config/routes.rb
+++ config/routes.rb
@@ -1,5 +1,6 @@
 Rails.application.routes.draw do
   # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
+  resources :posts, only: [:new]
   resources :authors, only: [:show, :index] do
   resources :posts, only: [:show, :index, :new, :edit]
 end
